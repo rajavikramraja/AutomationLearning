cdc15bda540414819a04b676b2d6ee44243c80f3
vikra <vikra@192.168.1.7>
2025-07-16 09:04:52 +0530

    Initial Commit

---
+++ AutomationLearn/src/org/javaInterview/ReverseStringLoop.java
@@ -0,0 +1,17 @@
+package org.javaInterview;
+
+import java.util.Scanner;
+
+public class ReverseStringLoop {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String nextLine = sc.nextLine();
+	String reverseValue="";
+	//For reverse need to iterate from last
+	for(int i=nextLine.length()-1; i>=0;i--) {
+		//empty string + chartAt -- ""+m=m
+			reverseValue=reverseValue+nextLine.charAt(i);	
+	}
+	System.out.println("Reverse String "+ reverseValue );
+}
+}
