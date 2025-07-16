00615157268761d9556407f5cd5b8908833d6156
vikra <vikra@192.168.1.7>
2025-07-16 22:32:05 +0530

    String Add

--- AutomationLearn/src/org/javaInterview/ReverseStringLoop.java
+++ AutomationLearn/src/org/javaInterview/ReverseStringLoop.java
@@ -3,9 +3,13 @@ package org.javaInterview;
 import java.util.Scanner;
 
 public class ReverseStringLoop {
+
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the String");
+	
 	String nextLine = sc.nextLine();
+	
 	String reverseValue="";
 	//For reverse need to iterate from last
 	for(int i=nextLine.length()-1; i>=0;i--) {
